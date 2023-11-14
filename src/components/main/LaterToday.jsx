a9cc4fc21728b0aad4283dc3c27cbad39491250c
ItayMizrachi <itaymiz98@gmail.com>
2023-11-14 14:44:46 +0200

    autohide scrollbar

--- src/components/main/LaterToday.jsx
+++ src/components/main/LaterToday.jsx
@@ -7,7 +7,7 @@ const LaterToday = ({ hourlyData }) => {
     <div>
       <h2 className="font-semibold text-xl my-5 ml-2">Today At</h2>
       <div className="overflow-hidden rounded-lg">
-        <SimpleBar style={{ maxHeight: '100%', overflowX: 'auto' }} autoHide={false}>
+        <SimpleBar style={{ maxHeight: '100%', overflowX: 'auto' }} autoHide={true}>
           <div className="flex space-x-5">
             {hourlyData?.map((item, index) => (
               <HourlyDataItem key={index} item={item} />
